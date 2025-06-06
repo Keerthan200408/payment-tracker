8bc2e3f4ce76ce75f505a9ac226116a18f8e851d
Keerthan200408 <2022eeb1226@iitrpr.ac.in>
2025-06-06 15:30:03 +0530

    frontend working checks3.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash][extname]`,
+      },
+    },
   },
   server: {
     port: 5173,
